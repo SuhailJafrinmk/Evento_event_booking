6ca5c7499947e87749d652f272e1cdd5b4a7e0d9
suhailjafrin <suhailmk196@gmail.com>
2025-01-01 20:11:38 +0530

    some random commit

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -31,6 +31,7 @@ class _HomePageState extends State<HomePage> {
       ..add(FetchEventByLocation());
       
   }
+  //wjdwduqhuhduhduhwuhdqiuhwuhdquhwudh
   @override
   Widget build(BuildContext context) {
     final theme=Theme.of(context);
